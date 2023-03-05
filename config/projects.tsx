b3a803515d4f2a2fd06870bfecee2ede39f68877
crlscontreras <31496905+crlscontreras@users.noreply.github.com>
2023-03-05 17:56:45 -0300

    update project page

--- config/projects.tsx
+++ config/projects.tsx
@@ -10,9 +10,25 @@ export const projects: Project[] = [
   {
     title: "Portfolio",
     gitUrl: "https://github.com/crlscontreras/portfolio-nextjs",
-    banner: "/static/images/webBanner.PNG",
+    banner: "/static/images/portfolio.PNG",
     website: "https://github.com/crlscontreras/portfolio-nextjs",
     description:
       "The webapp you are seeing right now! You can visit the repository to learn more about it.",
+  },
+  {
+    title: "SpringBoot REST API",
+    gitUrl: "https://github.com/crlscontreras/SpringApiRestTest",
+    banner: "/static/images/apiRest.PNG",
+    website: "https://github.com/crlscontreras/SpringApiRestTest",
+    description:
+      "REST API developed with Spring Boot, with unit tests made with JUnit5. Connected to a MongoDB Atlas database, which contains information from different movies and TV shows",
+  },
+  {
+    title: "Capstone Project ",
+    gitUrl: "https://github.com/crlscontreras/SpringApiRestTest",
+    banner: "/static/images/capstone.PNG",
+    website: "https://github.com/crlscontreras/SpringApiRestTest",
+    description:
+      "The final year assignment to obtain my college degree, in which I worked with the Chilean Health Service to design and build a web application.",
   },
 ];
